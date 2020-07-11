7fc0fd3a5576f3ec0a003bda133f7b6613a79347
psych0v0 <1102433962@qq.com>
2020-07-11 23:02:13 +0800

    请不要直接commit到master分支，首先建自己的分支，然后任何改变commit到那边。 如果你代码没有任何问题，建pull request，这样就可以把你的更改合并到master。 所有任务请在projects列出。 如果你有任何maven的问题，查看Preferences -> Build, Execution, Deployment -> Build Tools -> Maven -> Repositories 如果有红的URL，回来到Build Tools -> Maven 更改你的maven路径和什么相关的设置。

---
+++ src/main/java/com/xmu/mapper/OrderRelationMapper.java
@@ -0,0 +1,16 @@
+package com.xmu.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+@Mapper
+@Repository
+public interface OrderRelationMapper {
+
+    //查询订单，关联多表
+    List<Map<String,Object>> selectOrder();
+    List<Map<String,Object>> selectByUId(long uId);
+    List<Map<String,Object>> selectByHId(long hId);
+}
