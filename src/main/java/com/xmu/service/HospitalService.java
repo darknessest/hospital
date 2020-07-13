e8d27518f9b985a8974182b74e7c6707efb7b344
psych0v0 <1102433962@qq.com>
2020-07-13 23:51:59 +0800

    修改了之前代码的错误 更新了医院管理员的controller层和相应的jsp文件

--- src/main/java/com/xmu/service/HospitalService.java
+++ src/main/java/com/xmu/service/HospitalService.java
@@ -5,6 +5,7 @@ import com.xmu.entity.Hospital;
 import java.util.List;
 
 public interface HospitalService {
+    Hospital login(String hAccount,String hPassword);
     Hospital findById(long id);
     List<Hospital> findAll();
     int add(Hospital hospital);
--- src/main/java/com/xmu/service/impl/HospitalServiceImpl.java
+++ src/main/java/com/xmu/service/impl/HospitalServiceImpl.java
@@ -1,16 +1,31 @@
 package com.xmu.service.impl;
 
 import com.xmu.entity.Hospital;
+import com.xmu.entity.HospitalExample;
 import com.xmu.mapper.HospitalMapper;
 import com.xmu.service.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class HospitalServiceImpl implements HospitalService {
 
     @Autowired
     private HospitalMapper hospitalMapper;
+
+    @Override
+    public Hospital login(String hAccount, String hPassword) {
+        HospitalExample he = new HospitalExample();
+        he.createCriteria().andHAccountEqualTo(hAccount).andHPasswordEqualTo(hPassword);
+        List<Hospital> list = hospitalMapper.selectByExample(he);
+        if (list.size()==0){
+            return null;
+        }else{
+            return list.get(0);
+        }
+    }
 
     @Override
     public Hospital findById(long id) {
